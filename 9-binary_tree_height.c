adacbcf8ce48604fe44761a4be954684262faa8b
keigi14 <kbndbrn5@gmail.com>
2024-03-26 15:05:36 +0000

    change variables and created file

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t u = 0, v = 0;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		u = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		v = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((u > v) ? u : v);
 	}
